a62a1ece211e565b29408ba2dc878cf690790974
Alice Brunel <alc.brunel@gmail.com>
2019-07-12 08:58:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, data|
-    person == "Freddy Mercury"
-    data.each do   
+  contacts.each do |person, contact_details_hash|
+    binding.pry
   end
 end
